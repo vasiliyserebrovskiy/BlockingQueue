9c06e3aa7ee612865117b452710dc04c899a110b
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-06-12 14:18:47 +0200

    methods ready

--- src/ait/mediation/BlkQueueImpl.java
+++ src/ait/mediation/BlkQueueImpl.java
@@ -1,20 +1,67 @@
 package ait.mediation;
 
+import java.util.LinkedList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BlkQueueImpl<T> implements BlkQueue<T> {
+
+    private final int capacity;
+    private LinkedList<T> messages;
+
+    //Lock and conditions variables
+    private final Lock mutex = new ReentrantLock();
+    private final Condition pushWaitCondition = mutex.newCondition();
+    private final Condition popWaitCondition = mutex.newCondition();
+
     public BlkQueueImpl(int maxSize) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+        this.capacity = maxSize;
+        this.messages = new LinkedList<>();
+    }
+
+
+    //Usages if messages.size < capacity
+
+    // .add(element);    добавляет в конец (по умолчанию)
+    // .removeFirst();  Удаляет и возвращает первый элемент
+    @Override
+    public void push(T message) { // post in messageBox
+       mutex.lock();
+       try {
+           // if we did not have free space in messages list we need to wait
+           while(this.messages.size() == capacity) {
+               try {
+                   pushWaitCondition.await();
+               } catch (InterruptedException e) {
+                   throw new RuntimeException(e);
+               }
+           }
+           this.messages.addLast(message);
+           popWaitCondition.signal();
+       } finally {
+           mutex.unlock();
+       }
     }
 
     @Override
-    public void push(T message) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
-    }
+    public T pop() { // get in messageBox
+        mutex.lock();
+        try {
+            // if we did not have any message in messages list we need to wait
+            while(messages.isEmpty()) {
+                try {
+                    popWaitCondition.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            T res = messages.removeFirst();
+            pushWaitCondition.signal();
+            return res;
 
-    @Override
-    public T pop() {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+        } finally {
+            mutex.unlock();
+        }
     }
 }
